18183d3637be71acd76eb1b51c93574474b47827
nyash <alex-nyashin1996@yandex.ru>
2022-09-16 22:06:52 +0300

    update test and add new photo for test

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,37 +1,53 @@
+import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
     @BeforeAll
-    static void setUp(){
+    static void setUp() {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.browser = "edge";
         Configuration.holdBrowserOpen = true;
     }
 
     @Test
-    void fillPracticeForm(){
+    void fillPracticeForm() {
 
         //Тестовые данные
         String firstName = "Alex";
         String lastName = "Nyashin";
         String userEmail = "nyashin@test.com";
         String gender = "Male";
-        String mobile = "890547854720";
+        String mobile = "8905478547";
         String dateOfBirth = "07.07.1996";
         String subjects = "xm";
+        String hobbies = "Sports";
+        String address = "Moscow";
+        String state = "NCR";
 
         open("/automation-practice-form");
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $("[value=" + gender + "]").selectRadio("male");
+        $("[for=gender-radio-1]").click(); //мне не нравится этот селектор. Как по нему понять, что выбрат именно Male?
+        $("#userNumber").setValue(mobile);
+        $("[class=react-datepicker__input-container]")
+                .$("[id=dateOfBirthInput]")
+                .setValue(Keys.CONTROL + "a")
+                .sendKeys(dateOfBirth + Keys.ENTER);//это уж совсем костыль. Можно по-другому как-нибудь?)
+        $("#subjectsContainer").$("[autocapitalize=none]").setValue(subjects);
+        $(byText(hobbies)).click();
+        $("input#uploadPicture").uploadFile(new File("src/test/resources/test_cat_qa.jpg"));//тут загулил, так же можно сделать?)
+        $("#currentAddress").setValue(address);
+        $("#state").click();//$("[class= css-26l3qy-menu]").click();
     }
 }
